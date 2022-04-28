2d8705246b1f3e1210805d1d05bb3642a25cc8f7
nguyen30012022 <>
2022-04-28 14:02:47 +0900

    first commit

---
+++ build/generated-sources/ap-source-output/entities/Testid_.java
@@ -0,0 +1,15 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-04-28T13:45:21")
+@StaticMetamodel(Testid.class)
+public class Testid_ { 
+
+    public static volatile SingularAttribute<Testid, String> name;
+    public static volatile SingularAttribute<Testid, Integer> mess;
+    public static volatile SingularAttribute<Testid, Integer> id;
+
+}
